4488f4ecc352a22349c549aeb5f9b40f611e6fb3
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2019-08-06 11:10:29 -0700

    Add viewport height values

--- tailwind.config.js
+++ tailwind.config.js
@@ -90,7 +90,40 @@ module.exports = {
       tangerine: generateColors('#FCB040'),
       fog: generateColors('#E7E8EA')
     },
-    extend: {}
+    extend: {
+      height: {
+        'screen-1': '1vh',
+        'screen-2': '2vh',
+        'screen-3': '3vh',
+        'screen-4': '4vh',
+        'screen-5': '5vh',
+        'screen-6': '6vh',
+        'screen-8': '8vh',
+        'screen-10': '10vh',
+        'screen-12': '12vh',
+        'screen-16': '16vh',
+        'screen-20': '20vh',
+        'screen-24': '24vh',
+        'screen-32': '32vh',
+        'screen-40': '40vh',
+        'screen-48': '48vh',
+        'screen-56': '56vh',
+        'screen-64': '64vh',
+        'screen-68': '68vh',
+        'screen-72': '72vh',
+        'screen-76': '76vh',
+        'screen-80': '80vh',
+        'screen-84': '84vh',
+        'screen-88': '88vh',
+        'screen-90': '90vh',
+        'screen-92': '92vh',
+        'screen-94': '94vh',
+        'screen-95': '95vh',
+        'screen-96': '96vh',
+        'screen-98': '98vh',
+        'screen-99': '99vh'
+      }
+    }
   },
   variants: {
     borderWidth: ['responsive', 'hover', 'focus']
