761751b4b3f8e3049bcdc2a96c45f312b128e2fd
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2019-10-29 12:33:39 -0700

    Formatting changes for eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,31 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "plugin:vue/essential",
-        "airbnb-base"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["plugin:vue/essential", "plugin:vue/recommended", "airbnb-base"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    parser: "@typescript-eslint/parser",
+    sourceType: "module"
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {
+    "import/no-unresolved": 0,
+    "import/no-unassigned-import": 0,
+    semi: ["error", "never"],
+    "no-console": "off",
+    "space-before-function-paren": [
+      "error",
+      {
+        anonymous: "always",
+        named: "always",
+        asyncArrow: "always"
+      }
+    ]
+  }
 };
