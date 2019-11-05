42383ecd3bf8a424c36e0fc6b251d18880931735
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2019-11-05 14:28:06 -0800

    TSP-64 Add component for two column layout

---
+++ source/_patterns/03-cells/layouts/TwoColumn.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-row flex-wrap mx-auto">
+    <section :class="[defaultWidth, computedWidth, padding]">
+      <slot name="first"></slot>
+    </section>
+    <section :class="[computedWidth, padding]">
+      <slot name="second"></slot>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TwoColumn',
+  props: ['breakpoint', 'firstWidth', 'secondWidth'],
+  data() {
+    return {
+      defaultWidth: 'w-full',
+      padding: 'px-2',
+    };
+  },
+  computed: {
+    computedWidth() {
+      return [this.defaultWidth, this.firstWidth, this.secondWidth];
+    },
+  },
+};
+</script>
