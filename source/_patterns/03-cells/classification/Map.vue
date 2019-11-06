b863d663fc2f2d96b646babd35ea41d464b25544
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2019-11-06 08:41:46 -0800

    TSP-64 Add a legend to the map

--- source/_patterns/03-cells/classification/Map.vue
+++ source/_patterns/03-cells/classification/Map.vue
@@ -45,7 +45,30 @@
     </div>
     <div ref="bottom-right">
       <slot name="bottom-right">
-        <div class="h-64 w-64 border rounded overflow-y-auto bg-white">{{ extent }}</div>
+        <button v-if="!showLegend" class="h-8 w-8 p-1 bg-white" v-on:click="showLegend = true;">
+          <!-- This is from feather.  Their site is here: https://www.feathericons.com -->
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="h-6 w-6"
+          >
+            <polygon points="12 2 2 7 12 12 22 7 12 2" />
+            <polyline points="2 17 12 22 22 17" />
+            <polyline points="2 12 12 17 22 12" />
+          </svg>
+        </button>
+        <div v-show="showLegend" class="bg-white h-64 overflow-y-auto">
+          <header class="p-2 flex items-baseline justify-between sticky top-0 bg-gray-100">
+            <h2 class="m-0 text-2xl">Legend</h2>
+            <button v-on:click="showLegend = false;">X</button>
+          </header>
+          <main ref="legend" class="overflow-y-auto"></main>
+        </div>
       </slot>
     </div>
   </div>
@@ -55,61 +78,65 @@
 import Vue from "vue";
 
 import Map from "esri/Map";
-import watchUtils from "esri/core/watchUtils";
 import Extent from "esri/geometry/Extent";
 import Polyline from "esri/geometry/Polyline";
 import Graphic from "esri/Graphic";
 import GraphicsLayer from "esri/layers/GraphicsLayer";
-import SimpleFillSymbol from "esri/symbols/SimpleFillSymbol";
 import SimpleLineSymbol from "esri/symbols/SimpleLineSymbol";
 import MapView from "esri/views/MapView";
+import Legend from "esri/widgets/Legend";
 
 import Checkbox from "../../02-molecules/form/Checkbox.vue";
 
-import { layers, map } from "./data";
-
 import store from "../../../_data/tsp-store";
-
-const wkt = "";
 
 export default Vue.extend({
   name: "Map",
   data: function() {
     return {
-      extent: "",
-      layers,
-      showSettings: false
+      extent: store.state.extent,
+      layers: store.state.layers,
+      showSettings: false,
+      showLegend: false
     };
   },
   components: {
     Checkbox
   },
   mounted: function() {
-    const el = this.$refs["map"];
     const view = new MapView({
-      container: el,
+      container: this.$refs["map"],
       map: store.state.map
     });
 
-    view.extent = new Extent({
-      spatialReference: { wkid: 102100 },
-      xmin: -13656940.502309779,
-      ymin: 5703052.172158225,
-      xmax: -13656330.200412115,
-      ymax: 5703440.328942453
+    store.actions.setView(view);
+
+    store.actions.setExtent(
+      new Extent({
+        spatialReference: { wkid: 102100 },
+        xmin: -13656728.731995031,
+        ymin: 5703173.36547962,
+        xmax: -13656594.817904502,
+        ymax: 5703311.459720133
+      })
+    );
+
+    store.state.view.ui.add(this.$refs["top-right"], {
+      position: "top-right",
+      index: 2
     });
-
-    map.layers = this.layers;
-
-    view.ui.add(this.$refs["top-right"], "top-right");
-    view.ui.add(this.$refs["bottom-left"], "bottom-left");
-    view.ui.add(this.$refs["bottom-right"], {
+    store.state.view.ui.add(this.$refs["bottom-left"], {
+      position: "bottom-left",
+      index: 2
+    });
+    store.state.view.ui.add(this.$refs["bottom-right"], {
       position: "bottom-right",
       index: 2
     });
 
-    view.watch("extent", (newValue, oldValue, propertyName, target) => {
-      this.extent = JSON.stringify(newValue, null, 2);
+    const legend = new Legend({
+      view,
+      container: this.$refs["legend"]
     });
 
     const streetGraphic = new Graphic({
@@ -134,9 +161,7 @@ export default Vue.extend({
       graphics: [streetGraphic]
     });
 
-    store.state.map.layers.add(graphicsLayer);
-
-    view.goTo(streetGraphic);
+    store.actions.addLayer(graphicsLayer);
   }
 });
 </script>
