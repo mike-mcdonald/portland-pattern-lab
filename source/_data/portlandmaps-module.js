2d0bfb3d314a3489db365b2ea97e1bd8743be550
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2019-11-14 15:46:18 -0800

    TSP-66 Refactor address suggestion

--- source/_data/portlandmaps-module.js
+++ source/_data/portlandmaps-module.js
@@ -15,41 +15,54 @@ const mutations = {
   setCandidates(state, candidates) {
     state.candidates = candidates;
   }
-}
+};
 
 const actions = {
   clearCandidates({ commit }) {
     commit('setCandidates', undefined);
   },
   findCandidates({ commit, state }, search) {
-    axios.post(
-      'https://www.portlandmaps.com/api/suggest/',
-      qs.stringify({
-        query: search,
-        api_key: state.api_key,
-      }),
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
+    axios
+      .post(
+        'https://www.portlandmaps.com/api/suggest/',
+        qs.stringify({
+          query: search,
+          api_key: state.api_key
+        }),
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
         }
-      }
-    ).then((res) => {
-      if (res.status == 200 && res.data && res.data.status === 'success') {
-        const candidates = res.data.candidates;
+      )
+      .then(res => {
+        if (res.status == 200 && res.data && res.data.status === 'success') {
+          const candidates = res.data.candidates;
 
-        commit(
-          'setCandidates',
-          candidates.map((value) => {
-            value.location.spatialReference = res.data.spatialReference;
-            return value;
-          })
-        );
-      }
-    }).catch((err) => {
-      console.error(err);
-    })
+          commit(
+            'setCandidates',
+            candidates.map(value => {
+              let a = {
+                id: value.attributes.id,
+                location: value.location,
+                name: value.address,
+                city: value.attributes.city,
+                state: value.attributes.state,
+                zipCode: value.attributes.zip_code,
+                type: value.attributes.type,
+                county: value.attributes.county
+              };
+              a.location.spatialReference = res.data.spatialReference;
+              return a;
+            })
+          );
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
-}
+};
 
 export default {
   namespaced,
--- source/_patterns/04-organisms/address-search/AddressSuggest.vue
+++ source/_patterns/04-organisms/address-search/AddressSuggest.vue
@@ -5,10 +5,7 @@
         class="flex items-center border shadow rounded outline-none focus:shadow-outline "
         @submit.prevent="findCandidates(search)"
       >
-        <label
-          for="addressInput"
-          class="sr-only"
-        >Address</label>
+        <label for="addressInput" class="sr-only">Address</label>
         <input
           id="addressInput"
           v-model="search"
@@ -17,12 +14,9 @@
           placeholder="Enter an address to search"
           required="required"
           class="w-full px-3 py-2 rounded-l bg-fog-200"
-        >
+        />
         <section class="flex flex-row-reverse">
-          <button
-            aria-label="Search"
-            class="p-3"
-          >
+          <button aria-label="Search" class="p-3">
             <Search />
           </button>
           <transition name="fade">
@@ -30,7 +24,10 @@
               v-if="search"
               aria-label="Clear"
               class="p-3"
-              @click.stop.prevent="search = undefined; clearCandidates();"
+              @click.stop.prevent="
+                search = undefined;
+                clearCandidates();
+              "
             >
               <X />
             </button>
@@ -40,26 +37,23 @@
       <div class="-mt-1 p-1">
         <div
           v-for="candidate in candidates"
-          :key="candidate.attributes.id"
+          :key="candidate.id"
           class="w-full border-b border-l border-r"
           @click="handleClick(candidate)"
         >
-          <div
-            role="button"
-            href="#"
-            class="p-2 flex flex-wrap cursor-pointer hover:bg-blue-100"
-          >
+          <div role="button" href="#" class="p-2 flex flex-wrap cursor-pointer hover:bg-blue-100">
             <span
-              v-if="candidate.attributes.type"
+              v-if="candidate.type"
               class="flex items-center flex-initial rounded-full bg-blue-500 text-white uppercase px-2 py-1 text-xs font-bold mx-2"
-            >{{ candidate.attributes.type }}</span>
+              >{{ candidate.type }}</span
+            >
 
             <div class="mx-2">
-              {{ candidate.address }}
+              {{ candidate.name }}
             </div>
             <div class="flex flex-1 items-center mx-2">
-              <span class="uppercase text-xs font-bold">{{ candidate.attributes.city }},</span>
-              <span class="uppercase text-xs font-bold">{{ candidate.attributes.state }}</span>
+              <span class="uppercase text-xs font-bold">{{ candidate.city }},</span>
+              <span class="uppercase text-xs font-bold">{{ candidate.state }}</span>
             </div>
           </div>
         </div>
@@ -68,26 +62,25 @@
   </section>
 </template>
 <script lang="ts">
-import axios from "axios";
-import Vue from "vue";
+import axios from 'axios';
+import Vue from 'vue';
 import { mapState, mapActions } from 'vuex';
-import qs from "querystring";
+import qs from 'querystring';
 
-import Graphic from "esri/Graphic";
-import GraphicsLayer from "esri/layers/GraphicsLayer";
-import { Point, Polyline } from "esri/geometry";
+import Graphic from 'esri/Graphic';
+import GraphicsLayer from 'esri/layers/GraphicsLayer';
+import { Point, Polyline } from 'esri/geometry';
 
-import Search from "../../01-atoms/04-images/Search.vue";
-import X from "../../01-atoms/04-images/X.vue";
+import Search from '../../01-atoms/04-images/Search.vue';
+import X from '../../01-atoms/04-images/X.vue';
 
 export default Vue.extend({
-  name: "AddressSuggest",
+  name: 'AddressSuggest',
   components: {
     Search,
     X
   },
-  props: {
-  },
+  props: {},
   data() {
     return {
       search: undefined,
@@ -100,7 +93,7 @@ export default Vue.extend({
   methods: {
     ...mapActions('portlandmaps', ['clearCandidates', 'findCandidates']),
     handleClick(candidate) {
-      this.$emit('candidate-select', { candidate , spatialReference: this.spatialReference })
+      this.$emit('candidate-select', candidate);
       this.clearCandidates();
     }
   }
