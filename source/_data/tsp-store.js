74eb50d56569a9a01067f6f706363ef4e40909bc
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2019-11-14 13:11:23 -0800

    TSP-66 Add demo store for address suggestion

--- source/_data/tsp-store.js
+++ source/_data/tsp-store.js
@@ -1,13 +1,20 @@
-import Basemap from "esri/Basemap";
-import FeatureLayer from "esri/layers/FeatureLayer";
-import TileLayer from "esri/layers/TileLayer";
-import GroupLayer from "esri/layers/GroupLayer";
-import EsriMap from "esri/Map";
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import Basemap from 'esri/Basemap';
+import FeatureLayer from 'esri/layers/FeatureLayer';
+import TileLayer from 'esri/layers/TileLayer';
+import GroupLayer from 'esri/layers/GroupLayer';
+import EsriMap from 'esri/Map';
+
+import portlandmaps from './portlandmaps-module';
+
+import secrets from './.secrets';
 
 export const basemaps = [
-  "https://www.portlandmaps.com/arcgis/rest/services/Public/Basemap_Color_Complete/MapServer",
-  "https://www.portlandmaps.com/arcgis/rest/services/Public/Basemap_Gray_Complete/MapServer",
-  "https://www.portlandmaps.com/arcgis/rest/services/Public/Basemap_Color_Complete_Aerial/MapServer"
+  'https://www.portlandmaps.com/arcgis/rest/services/Public/Basemap_Color_Complete/MapServer',
+  'https://www.portlandmaps.com/arcgis/rest/services/Public/Basemap_Gray_Complete/MapServer',
+  'https://www.portlandmaps.com/arcgis/rest/services/Public/Basemap_Color_Complete_Aerial/MapServer'
 ].map(
   url =>
     new Basemap({
@@ -16,20 +23,20 @@ export const basemaps = [
           url
         })
       ],
-      title: /Basemap_([a-zA-Z_]+)/i.exec(url)[1].replace("_", " ")
+      title: /Basemap_([a-zA-Z_]+)/i.exec(url)[1].replace('_', ' ')
     })
 );
 
 export const layers = [
   new GroupLayer({
-    id: "transit-classifications",
-    title: "Transit classes",
-    visibilityMode: "inheirited",
+    id: 'transit-classifications',
+    title: 'Transit classes',
+    visibilityMode: 'inheirited',
     visible: true,
     layers: [
-      "https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/1",
-      "https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/2",
-      "https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/3"
+      'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/1',
+      'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/2',
+      'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/3'
     ].map(
       url =>
         new FeatureLayer({
@@ -39,17 +46,17 @@ export const layers = [
   }),
   new FeatureLayer({
     url:
-      "https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/4",
+      'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/4',
     visible: false
   }),
   new GroupLayer({
-    id: "emergency-classifications",
-    title: "Emergency response classes",
-    visibilityMode: "inheirited",
+    id: 'emergency-classifications',
+    title: 'Emergency response classes',
+    visibilityMode: 'inheirited',
     visible: false,
     layers: [
-      "https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/6",
-      "https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/7"
+      'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/6',
+      'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/7'
     ].map(
       url =>
         new FeatureLayer({
@@ -58,13 +65,13 @@ export const layers = [
     )
   }),
   new GroupLayer({
-    id: "street-design-classifications",
-    title: "Street design classes",
-    visibilityMode: "inheirited",
+    id: 'street-design-classifications',
+    title: 'Street design classes',
+    visibilityMode: 'inheirited',
     visible: false,
     layers: [
-      "https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/9",
-      "https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/10"
+      'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/9',
+      'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/10'
     ].map(
       url =>
         new FeatureLayer({
@@ -73,13 +80,13 @@ export const layers = [
     )
   }),
   new GroupLayer({
-    id: "bicycle-classifications",
-    title: "Bicycle classes",
-    visibilityMode: "inheirited",
+    id: 'bicycle-classifications',
+    title: 'Bicycle classes',
+    visibilityMode: 'inheirited',
     visible: false,
     layers: [
-      "https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/12",
-      "https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/13"
+      'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/12',
+      'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/13'
     ].map(
       url =>
         new FeatureLayer({
@@ -88,13 +95,13 @@ export const layers = [
     )
   }),
   new GroupLayer({
-    id: "pedestrian-classifications",
-    title: "Pedestrian classes",
-    visibilityMode: "inheirited",
+    id: 'pedestrian-classifications',
+    title: 'Pedestrian classes',
+    visibilityMode: 'inheirited',
     visible: false,
     layers: [
-      "https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/15",
-      "https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/16"
+      'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/15',
+      'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/16'
     ].map(
       url =>
         new FeatureLayer({
@@ -103,14 +110,14 @@ export const layers = [
     )
   }),
   new GroupLayer({
-    id: "freight-classifications",
-    title: "Freight classes",
-    visibilityMode: "inheirited",
+    id: 'freight-classifications',
+    title: 'Freight classes',
+    visibilityMode: 'inheirited',
     visible: false,
     layers: [
-      "https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/18",
-      "https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/19",
-      "https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/20"
+      'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/18',
+      'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/19',
+      'https://www.portlandmaps.com/arcgis/rest/services/Public/Transportation_System_Plan/MapServer/20'
     ].map(
       url =>
         new FeatureLayer({
@@ -127,31 +134,39 @@ export const map = new EsriMap({
 
 const state = {
   debug: true,
-  message: "Hello!",
+  message: 'Hello!',
   view: null,
+  portlandmaps_api_key: secrets.portlandmapsApiKey,
   basemaps,
   map,
   layers
 };
 
-const actions = {
-  setView (view) {
+const mutations = {
+  setView(view) {
     state.view = view;
   },
-  setExtent (extent) {
+  setExtent(extent) {
     if (state.view) {
       state.view.extent = extent;
     }
   },
-  addLayer (layer) {
+  addLayer(layer) {
     state.map.add(layer);
   },
-  addGraphic (graphic) {
+  addGraphic(graphic) {
     state.view.graphics.add(graphic);
   }
 };
 
-export default {
+Vue.use(Vuex);
+
+const store = {
   state,
-  actions
+  mutations,
+  modules: {
+    portlandmaps
+  }
 };
+
+export default new Vuex.Store(store);
