cc72ad7134f800ab6fed74a778d4907d55f81e20
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2019-11-18 17:40:50 -0800

    TSP-76 Add footer component

---
+++ source/_patterns/03-cells/footer/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer
+    class="flex flex-col md:flex-row flex-wrap items-center justify-center md:justify-between bg-gray-300 text-black p-2 border-black border-t"
+    role="banner"
+  >
+    <slot name="logos">
+      <img
+        title="PBOT Technology services logo"
+        alt="PBOT Technology services logo"
+        src="/images/PBOT-VerticalApps-Logo-Dark.png"
+        class="w-48 md:w-64"
+      />
+    </slot>
+    <slot></slot>
+    <slot name="copyright">
+      <p class="m-0">© City of Portland, Oregon {{ new Date().getFullYear() }}</p>
+    </slot>
+  </footer>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'Footer'
+});
+</script>
